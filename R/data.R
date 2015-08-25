a646c6415d8b6fec07a4133580202683e1a0cc96
Thomas McMahon <thmcmahon@gmail.com>
2015-08-25 16:40:29 +0200

    Updated documentation

--- R/data.R
+++ R/data.R
@@ -1,11 +1,34 @@
 #' Super Rugby results.
 #'
-#' Results of all Super Rugby matches from 1996 - 2015. Thanks to Kevin Lassen
-#' for meticulously collating the original database. These results were scraped
-#' from http://www.lassen.co.nz/.
+#' Results of all Super Rugby matches from 1996 - 2015.
 #'
-#' @format A data frame with sixteen variables: \code{comp}, \code{season},
-#'   \code{round}, \code{date}, \code{time}, \code{country}, \code{city},
-#'   \code{home}, \code{away}, \code{home_score}, \code{away_score}, \code{home_tries},
-#'   \code{away_tries}, \code{home_goals}, \code{away_goals}, \code{result}.
+#'
+#' Thanks to Kevin Lassen for meticulously collating the original database.
+#'
+#' @source Kevin Lassen, \url{http://www.lassen.co.nz/s14tab.php}.
+#'
+#' @usage
+#'  data(superrugby)
+#'
+#' @format A data frame with sixteen variables:
+#' \describe{
+#'  \item{comp}{Competition name e.g. Super 12.}
+#'  \item{season}{Competition year}
+#'  \item{round}{Competition round}
+#'  \item{date}{Fixture date}
+#'  \item{time}{Fixture time. This is missing from 1996 to 2002.}
+#'  \item{country, city}{Location of fixture}
+#'  \item{home, away}{Home and away team names}
+#'  \item{home_score, away_score}{Total points of home and away teams}
+#'  \item{home_tries, away_tries}{Total tries of home and away teams}
+#'  \item{home_points, away_points}{Competition points for home and away teams.
+#'  4 points for a win, 2 for a draw, 1 for scoring four or more tries and 1 for
+#'  losing by seven points or less.}
+#'  \item{result}{Fixture result}
+#' }
+#'
+#' @examples
+#' # Show the Super 12 results from Round 1 of Season 2003
+#' data(superrugby)
+#' superrugby[superrugby$round==1 & superrugby$season==2003,]
 "superrugby"
