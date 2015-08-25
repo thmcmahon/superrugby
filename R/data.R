1d204ba92523b8f7538cf62a07dcdd775709fd0e
Thomas McMahon <thmcmahon@gmail.com>
2015-08-25 15:45:43 +0200

    First commit

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Super Rugby results.
+#'
+#' Results of all Super Rugby matches from 1996 - 2015. Thanks to Kevin Lassen
+#' for meticulously collating the original database. These results were scraped
+#' from http://www.lassen.co.nz/.
+#'
+#' @format A data frame with sixteen variables: \code{comp}, \code{season},
+#'   \code{round}, \code{date}, \code{time}, \code{country}, \code{city},
+#'   \code{home}, \code{away}, \code{home_score}, \code{away_score}, \code{home_tries},
+#'   \code{away_tries}, \code{home_goals}, \code{away_goals}, \code{result}.
+"superrugby"
---
+++ data-raw/superrugby.R
@@ -0,0 +1,61 @@
+library(dplyr)
+library(rvest)
+library(tidyr)
+library(lubridate)
+
+get_sr_table <- function(year) {
+  # Downloads the results for a given year
+  base_url <- 'http://www.lassen.co.nz/s14mat.php?'
+  url <- paste(base_url, "year=", year, sep = "")
+  table <- html(url) %>% html_nodes('table') %>% html_table(fill = TRUE)
+  table[[2]] # element 2 is the actual results, the others are metadata
+}
+
+if (!file.exists('data-raw/superrugby_raw.csv')) {
+  # If the dataset is not present then download for each year of the
+  # professional era and save the dataset as a csv file
+  superrugby_raw <- do.call(rbind, lapply(c(1996:2015), get_sr_table))
+  write.csv(superrugby_raw, file = 'data-raw/superrugby_raw.csv',
+            row.names = FALSE)
+  rm(superrugby_raw)
+}
+
+superrugby <- read.csv('data-raw/superrugby_raw.csv')
+
+# Tidy up the data
+names(superrugby) <- tolower(names(superrugby))
+
+# Dates
+superrugby$date <- dmy(superrugby$date)
+superrugby$season <- as.factor(year(superrugby$date))
+
+# Use the IOC encoding for country
+levels(superrugby$`in`) <- c('AUS', 'NZL', 'RSA')
+
+# Reorganise and normalise merged columns
+superrugby <- superrugby %>%
+  tbl_df %>%
+  # Normalise merged columns
+  separate(col = match, into = c('home', 'away'),
+           sep = ' v ', convert = TRUE) %>%
+  separate(col = score, into = c('home_score', 'away_score'),
+           sep = '-', convert = TRUE) %>%
+  separate(col = tries, into = c('home_tries', 'away_tries'),
+           sep = ':', convert = TRUE) %>%
+  separate(col = points, into = c('home_goals', 'away_goals'),
+           sep = "-", convert = TRUE) %>%
+  # Reorganise and rename
+  select(comp, season, round, date, time, country = `in`, city = adv, home,
+         away, home_score, away_score, home_tries, away_tries, home_goals,
+         away_goals)
+
+# Add a results column
+superrugby$result <-
+  ifelse(superrugby$home_score == superrugby$away_score, 'Draw',
+         ifelse(superrugby$home_score > superrugby$away_score, 'Home Win',
+                'Away Win')
+         )
+
+superrugby$result <- as.factor(superrugby$result)
+
+save(superrugby, file = 'data/superrugby.rdata', compress = "xz")
